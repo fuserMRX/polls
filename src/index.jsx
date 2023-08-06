d4e78e8b7de5629484e408ae2a777111a2370b89
ast_vnovodid <v.novodid@astoundcommerce.com>
2023-08-06 13:43:50 +0300

    add: basic updated structure

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import App from './components/App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from './components/App';
+import './index.css';
 
 function ColorfulBorder() {
   return (
-    <React.Fragment>
+    <>
       <ul className='border-container'>
         <li className='border-item' style={{ background: 'var(--red)' }} />
         <li className='border-item' style={{ background: 'var(--blue)' }} />
@@ -13,14 +13,15 @@ function ColorfulBorder() {
         <li className='border-item' style={{ background: 'var(--yellow)' }} />
         <li className='border-item' style={{ background: 'var(--aqua)' }} />
       </ul>
-    </React.Fragment>
+    </>
   )
 }
 
-ReactDOM.render(
-  <React.Fragment>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
     <ColorfulBorder />
     <App />
-  </React.Fragment>,
-  document.getElementById('root')
-)
+  </React.StrictMode>
+);
