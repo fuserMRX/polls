8f42e452ae0f63609a81c238106e5699e33bdecf
ast_vnovodid <v.novodid@astoundcommerce.com>
2023-08-07 21:10:57 +0300

    add: add answers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { loadingBarReducer } from 'react-redux-loading-bar';
 import authedUser from './authedUser';
 import users from './users';
+import polls from './polls';
 
 export default combineReducers({
     authedUser,
     users,
+    polls,
     loadingBar: loadingBarReducer
 });
